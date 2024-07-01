8ef6ea889e17f74c5ca4c9d8f38b57b0d2ef7593
Shubham Rahangdale <102543960+Shubhamrahangdale@users.noreply.github.com>
2024-07-01 08:38:35 +0530

    Update main.tf

--- .github/env/prod/main.tf
+++ .github/env/prod/main.tf
@@ -30,13 +30,6 @@ resource "aws_security_group" "app_sg" {
   }
   
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 1337
     to_port     = 1337
     protocol    = "tcp"
